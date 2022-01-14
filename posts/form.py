7bdec099b74c8b36eab408ece26db912fb397492
Anatolii <kent0806@mail.ru>
2022-01-14 21:20:00 +0300

    add CKEditor in templates

--- posts/form.py
+++ posts/form.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.widgets import TextArea
 from wtforms.validators import DataRequired
+from flask_ckeditor import CKEditorField
 
 
 class PostForm(FlaskForm):
     title = StringField('Название', validators=[DataRequired()])
-    content = StringField('Контент', validators=[
-        DataRequired()], widget=TextArea())
+    content = CKEditorField('Контент', validators=[DataRequired()])
     slug = StringField('Slug', validators=[DataRequired()])
     submit = SubmitField('Отправить')
